3bfea1bd469d1e8f31f32828f81f512338a78b06
Aakriti <anchij8@gmail.com>
2020-10-13 18:17:00 +0530

    initial commit

---
+++ src/app.js
@@ -0,0 +1,77 @@
+const path = require('path')
+const express = require('express')
+const hbs =  require('hbs')
+const geocode=require('./utils/geocode')
+const forecast=require('./utils/forecast')
+const app = express()
+
+//define paths for express config
+const viewPath=path.join(__dirname,'../templates/views')
+const partialsPath=path.join(__dirname,'../templates/partials')
+
+//handlebarsetups
+app.set('view engine','hbs')
+app.set('views',viewPath)
+hbs.registerPartials(partialsPath)
+
+app.use(express.static(path.join(__dirname,'../public')))
+
+app.get('',(req,res)=>{
+    res.render('index',{
+        title:'weather app',
+        name:"andrew mead"
+    })
+})
+
+app.get('/about',(req,res)=>{
+    res.render('about',{
+        title:'About',
+        name:'Aakriti'
+    })
+})
+
+app.get('/weather',(req,res)=>{
+    if(!req.query.address){
+        return res.send({
+            error:"ERROR!!!"
+        })
+    }
+    geocode(req.query.address,(error,{latitude,longitude,location}={})=>{
+        if(error){
+            return res.send({error})
+        }
+        forecast(latitude,longitude,(error,forecastData)=>{
+            if(error){
+                return res.send({error})
+            }
+            res.send({
+                forecast:forecastData,
+                location,
+                address:req.query.address
+
+            })
+        })
+    })
+
+})
+
+
+app.get('/about/*',(req,res)=>{
+    res.render('404',{
+        title:"generic 404",
+        name:"by end",
+        errorMessage:"not foundd"
+    })
+
+})
+app.get('*',(req,res)=>{
+    res.render('404',{
+        title:'Error Page',
+        name:'By ANd',
+        errorMessage:"Page Not Found"
+    })
+})
+
+app.listen(3000,()=>{
+    console.log('Server is up port 3000')
+})
