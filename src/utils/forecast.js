9c984f84627c48ecfdff983f0f4580771bc32d28
Aakriti <anchij8@gmail.com>
2020-10-14 18:54:15 +0530

    Added humidity  to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,10 @@ const forecast = (latitude,longitude,callback)=>{
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined,body.current.weather_descriptions[0] +", it is currently "+body.current.temperature+" but it feels like "+body.current.feelslike)
+            callback(undefined,
+                body.current.weather_descriptions[0] +", it is currently "+body.current.temperature+
+                " but it feels like "+body.current.feelslike+
+                " humidity today is "+ body.current.humidity + "%")
         }
     })
 }
